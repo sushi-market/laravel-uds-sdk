49be7c2d1f8026294bfaafbe4c659cbfb4359f7d
Nikolay <npisarev16@gmail.com>
2023-10-27 14:13:19 +0600

    Логирование запросов в виде Json

---
+++ src/Guzzle/MessageFormatterJson.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace SushiMarket\UdsSdk\Guzzle;
+
+use GuzzleHttp\MessageFormatterInterface;
+use Psr\Http\Message\RequestInterface;
+use Psr\Http\Message\ResponseInterface;
+
+class MessageFormatterJson implements MessageFormatterInterface
+{
+
+    public function format(RequestInterface $request, ResponseInterface $response = null, \Throwable $error = null): string
+    {
+        return json_encode([
+            'request_headers' => $request->getHeaders(),
+            'request_body' => $request->getBody()->getContents(),
+            'response_headers' => $response->getHeaders(),
+            'response_body' => $response->getBody()->getContents(),
+            'status_code' => $response->getStatusCode()
+        ]);
+    }
+}
