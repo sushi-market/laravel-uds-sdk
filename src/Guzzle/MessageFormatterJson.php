89d9a74835ae00b0986717ec22d04428c845639b
Nikolay <npisarev16@gmail.com>
2023-10-27 17:02:54 +0600

    Рефакторинг

--- src/Guzzle/MessageFormatterJson.php
+++ src/Guzzle/MessageFormatterJson.php
@@ -11,14 +11,17 @@ class MessageFormatterJson implements MessageFormatterInterface
 {
     public function format(RequestInterface $request, ResponseInterface $response = null, Throwable $error = null): string
     {
+        $request_body = $request->getBody()->getContents();
+        $response_body = $response->getBody()->getContents();
+
         return json_encode([
             'method' => $request->getMethod(),
             'url' => $request->getUri(),
             'status_code' => $response->getStatusCode(),
             'request_headers' => $request->getHeaders(),
-            'request_body' => $request->getBody()->getContents(),
+            'request_body' => json_validate($request_body) ? json_decode($request_body) : $request_body,
             'response_headers' => $response->getHeaders(),
-            'response_body' => $response->getBody()->getContents()
-        ]);
+            'response_body' => json_validate($response_body) ? json_decode($response_body) : $response_body,
+        ], JSON_UNESCAPED_UNICODE);
     }
 }
